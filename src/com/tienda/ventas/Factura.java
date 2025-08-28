21276a5ad4793da0872be1799d3b0b9bb85d3d5c
rafaelmartin <rafael.docencia23@gmail.com>
2025-08-28 12:19:47 +0100

    Correcciones en Factura

--- src/com/tienda/ventas/Factura.java
+++ src/com/tienda/ventas/Factura.java
@@ -1,20 +1,24 @@
 package com.tienda.ventas;
-
 // BUGS:
 // - Falta importar List y ArrayList (compila mal).
 // - Usa Producto sin importar el paquete.
 // - Posible división por cero si la lista está vacía.
-
-import com.tienda.productos.Producto;
-// falta: import java.util.List;
-// falta: import java.util.ArrayList;
+import java.util.*
 
 public class Factura {
-    private java.util.List<Producto> productos = new java.util.ArrayList<>(); // parche temporal
+
+//    private java.util.List<Producto> productos = new java.util.ArrayList<>(); // parche temporal
+    private List<Producto> productos = new ArrayList<>();
 
     public void agregarProducto(Producto p) {
+
         // BUG: no validar nulos (causa NullPointer más tarde)
-        productos.add(p);
+        if (p != null) {
+            productos.add(p);
+        } else {
+            System.out.println("Error, no hay producto que agregar");
+        }
+
     }
 
     public double calcularTotal() {
@@ -26,12 +30,21 @@ public class Factura {
     }
 
     public int calcularPromedioEntero() {
-        // BUG: división por cero si no hay productos
+
         double total = calcularTotal();
-        return (int) (total / productos.size());
+
+        // BUG: división por cero si no hay productos
+        if (productos !=0) {
+            return (int) (total / productos.size());
+        } else {
+            return 0;
+        }
     }
 
-    public java.util.List<Producto> getProductos() {
+    //public java.util.List<Producto> getProductos() {
+        return productos;
+    }
+    public List<Producto> getProductos() {
         return productos;
     }
 }
