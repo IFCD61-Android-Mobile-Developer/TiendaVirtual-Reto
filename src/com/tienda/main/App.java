d8cb81f4ff0d0dde560f5c0a6fd70abaa1d6e619
RafaelAI25 <rafael.docencia23@gmail.com>
2025-08-29 09:36:22 +0100

    actualización App

--- src/com/tienda/main/App.java
+++ src/com/tienda/main/App.java
@@ -1,7 +1,5 @@
 package com.tienda.main;
 
-
-
 import java.util.Scanner;
 import com.tienda.productos.Producto;
 import com.tienda.ventas.Factura;
@@ -9,36 +7,47 @@ import com.tienda.util.Calculadora;
 import com.tienda.excepciones.PrecioInvalidoException;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Factura factura = new Factura();
         Calculadora calc = new Calculadora();
 
+        if(args.length > 0){
+            System.out.println("Primer argumento: " + args[0]);
+        } else {
+            System.out.println("Sin argumentos: ejecuta con algún valor para probar");
+        }
 
-        System.out.println("Primer argumento: " + args[0]);
 
         try {
 
-            System.out.println("5 / 0 = " + calc.dividir(5, 0));
-        } catch (Exception e) {
-            System.out.println("Ocurrió un error en división: " + e.getMessage());
+            System.out.println("5 / 1 = " + calc.dividir(5, 1));
+        } catch (ArithmeticException ex) {
+            System.out.println("Ocurrió un error en división: " + ex.getMessage());
         }
 
         try {
-
-            Producto p1 = new Producto("Manzana", -1.5);
+            Producto p1 = new Producto("Manzana", 1.5);
+            Producto p2 = new Producto("Pera", 2.0);
+            factura.agregarProducto(p1);
             factura.agregarProducto(p1);
         } catch (PrecioInvalidoException e) {
             System.out.println("Precio inválido: " + e.getMessage());
         }
 
-
-        factura.agregarProducto(null);
-
         System.out.println("Total: " + factura.calcularTotal());
-        System.out.println("Promedio entero: " + factura.calcularPromedioEntero());
+        try {
+            System.out.println("Promedio entero: " + factura.calcularPromedioEntero());
+        } catch (IllegalStateException e) {
+            System.out.println("No hay productos para promedio: " + e.getMessage());
+        }
 
 
-        System.out.println("√(-9) = " + calc.raizCuadrada(-9));
+        try {
+            System.out.println("√(-9) = " + calc.raizCuadrada(9));
+        } catch (RaizNegativaException e){
+            System.out.println("Error con raíz: " + e.getMessage());
+        }
+
     }
 }
 
